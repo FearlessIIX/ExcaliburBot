40dd54d154882bbbda5c062dfcf1069de2402813
FearlessIIX <fearlessiix563@gmail.com>
2025-06-22 23:01:10 -0400

    ExcaliburBot 0.2.0    Made forward progress on user command

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, MessageFlags, AttachmentBuilder } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags, AttachmentBuilder, ClientVoiceManager } = require('discord.js');
 const Canvas = require("@napi-rs/canvas");
 const { token } = require('./config.json');
 
@@ -49,67 +49,87 @@ client.on(Events.InteractionCreate, async interaction => {
 
 	if (interaction.commandName === "user") {
 
-		let target = interaction.options.getUser("target")
-
-		if (!target) target = interaction.user
-
-		// Create the canvas for image manipulation, and grab its internal context
-		const canvas = Canvas.createCanvas(1000, 500)
-		const context = canvas.getContext("2d")
+		try {
+			const targetGuild = interaction.guild
 
 
-		// Grab the background image from the discords cdn servers
-		const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/1384813905678696519/1385447411631132762/castleBackgroundImage.png?ex=6856c2b3&is=68557133&hm=168b283f95652b4404da0ee7a7f6604d7e7165090182ea3f56fc2cfb476d988d&');
-		// Draw the image on the canvas context, ensuring its size is the same as the canvas
-		context.drawImage(background, 0, 0, canvas.width, canvas.height);
+			let target = await targetGuild.members.fetch(interaction.options.getUser("target").id)
+
+			if (!target) target = interaction.user
+
+			// Create the canvas for image manipulation, and grab its internal context
+			const canvas = Canvas.createCanvas(1000, 500)
+			const context = canvas.getContext("2d")
+
+			// Background Image ----------
+			// Grab the background image from the discords cdn servers
+			const background = await Canvas.loadImage('./assets/castleBackgroundImage.png');
+			// Draw the image on the canvas context, ensuring its size is the same as the canvas
+			context.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 
-		const avatarBarW = canvas.width / 3
-		// Set alpha to 1/3, then make a background for avatar to sit on
-		context.globalAlpha = 0.33
-		context.fillStyle = "##c9a6a5"
+			// Profile Picture Backing Bar ----------
+			const avatarBarW = canvas.width / 3
+			// Set alpha to 1/3, then make a background for avatar to sit on
+			context.globalAlpha = 0.33
+			context.fillStyle = "##c9a6a5"
 
-		context.fillRect(0, 0, avatarBarW, canvas.height)
+			context.fillRect(0, 0, avatarBarW, canvas.height)
 
-		context.globalAlpha = 1
-		// Draw line to separate avatar from the rest of the banner
-		context.fillRect(avatarBarW, 0, 3, canvas.height)
-
-
-		
-		// Calculate some needed coordinates and sizes for use with avatar placement
-		const avatarSize = canvas.width / 4
-		const avatarPosW = (avatarBarW / 2) - (avatarSize / 2)
-		const avatarPosH = (canvas.height / 2) - (avatarSize / 2)
-		const clippingRadius = avatarSize / 2
+			context.globalAlpha = 1
+			// Draw line to separate avatar from the rest of the banner
+			context.fillRect(avatarBarW, 0, 3, canvas.height)
 
 
-		// Grab the users avatar, and load it
-		const avatar = await Canvas.loadImage(target.displayAvatarURL({ extension: "png" }));
-
-		// Save the context so we can continue unclipped after drawing avatar
-		context.save()
-
-		// Clip the drawing area to just inside the circle
-		context.beginPath()
-		// Drawing circle the desired size and shape of avatar
-		context.arc(avatarPosW + (avatarSize / 2), avatarPosH + (avatarSize / 2), clippingRadius, 0, Math.PI * 2, true)
-		// Close the path and clip context.
-		context.closePath()
-		context.clip();
-
-		// Finally draw the avatar onto the canvas, limited by our previous clip. Then restore the old context
-		context.drawImage(avatar, avatarPosW, avatarPosH, avatarSize, avatarSize);
-		context.restore()
+			// Calculate some needed coordinates and sizes for use with avatar placement
+			const avatarSize = canvas.width / 4
+			const avatarPosW = (avatarBarW / 2) - (avatarSize / 2)
+			const avatarPosH = (canvas.height / 2) - (avatarSize / 2)
+			const clippingRadius = avatarSize / 2
 
 
-		// Build the final attachment for sending
-		const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
-		// Send the attachment
-		interaction.reply({ files: [attachment] });
+			// Target Avatar ----------
+			// Grab the users avatar, and load it
+			const avatar = await Canvas.loadImage(target.displayAvatarURL({ extension: "png" }));
 
-		// We are done processing this interaction
-		return;
+			
+
+			// Save the context so we can continue unclipped after drawing avatar
+			context.save()
+
+			// Clip the drawing area to just inside the circle
+			context.beginPath()
+			// Drawing circle the desired size and shape of avatar
+			context.arc(avatarPosW + (avatarSize / 2), avatarPosH + (avatarSize / 2), clippingRadius, 0, Math.PI * 2, true)
+			// Close the path and clip context.
+			context.closePath()
+			context.clip();
+
+			// Finally draw the avatar onto the canvas, limited by our previous clip. Then restore the old context
+			context.drawImage(avatar, avatarPosW, avatarPosH, avatarSize, avatarSize);
+			context.restore()
+
+
+			context.font = "40px serif"
+			const textWidth = context.measureText(target.displayName).width
+			const textPlacementLocation = (canvas.width / 3 + (canvas.width - canvas.width / 3) / 2) - textWidth / 2
+
+			context.fillText(target.displayName, textPlacementLocation, 50)
+
+
+			// Build the final attachment for sending
+			const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
+			// Send the attachment
+			interaction.reply({ files: [attachment] });
+
+			// We are done processing this interaction
+			return;
+		} catch(error) {
+			console.log(error)
+			interaction.followUp("There was an error while running this command")
+		}
+
+
 	}
 
 	if (!command) {
